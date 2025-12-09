c8429ed86ae9d80bdb24420e0b7ac0f2016baf2f
karishma <karishmar22042002@gmail.com>
2025-12-09 23:29:06 +0530

    updated

--- backend/routes/order.js
+++
@@ -1,5 +0,0 @@
-const express =require('express');
-const {createOrder} = require('../controllers/ordercontroller');
-const router = express.Router();
-router.route('/order').post(createOrder);
-module.exports=router;
